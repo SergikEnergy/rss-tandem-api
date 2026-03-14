74a47b1d69472417f53e94ff768f0a46cd94c479
skustov <SKustov@alfabank.ru>
2026-03-14 20:23:19 +0300

    fix(*): enable cors for deploy and localhost

--- src/common/cors.ts
+++ src/common/cors.ts
@@ -1,3 +1,11 @@
 export const METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
 
-export const ORIGINS = ['*'];
+export const ORIGINS = [
+    /^https?:\/\/(localhost)(:\d+)?$/,
+    /^https?:\/\/(127\.0\.0\.1)(:\d+)?$/,
+    /^https?:\/\/(0\.0\.0\.0)(:\d+)?$/,
+    /^https?:\/\/(\[::1\])(:\d+)?$/,
+    /^https?:\/\/(\[::1\])(:\d+)?$/,
+    'https://code-mind-one.vercel.app',
+    'http://code-mind-one.vercel.app',
+];
